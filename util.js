688bc3dd27b8521cf2d580a783c4019d00d81700
Nikolaus Piccolotto <prayerslayer@gmail.com>
2014-04-13 17:25:41 +0200

    random should be max inclusive

--- util.js
+++ util.js
@@ -1,6 +1,5 @@
 exports.random = function random( max, min ) {
 	if ( arguments.length === 1 ) min = 0;
-	if ( arguments.length === 0 ) max = 1;
-
-	return Math.floor( Math.random() * ( max - min ) + min );
+	if ( arguments.length === 0 ) return Math.random();
+	return Math.floor( Math.random() * ( max - min + 1 ) + min );
 };
