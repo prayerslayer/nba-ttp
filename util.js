c35fba2ec1c9cc8f618f1fd3dad3fecd3643a189
Nikolaus Piccolotto <prayerslayer@gmail.com>
2014-04-13 17:25:26 +0200

    remove obselete function

--- util.js
+++ util.js
@@ -1,12 +1,3 @@
-// transform [ 0, 2, 3, 5 ] to [ 0, 3, 5, 2]
-exports.shiftLeft = function shiftLeft( array, start ) {
-	var toShift = array.splice( start ),
-		firstOf = toShift.shift();
-
-	toShift.push( firstOf );
-	array.push.apply( array, toShift );
-};
-
 exports.random = function random( max, min ) {
 	if ( arguments.length === 1 ) min = 0;
 	if ( arguments.length === 0 ) max = 1;
